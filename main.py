e9eb94bbb06269814a42646b54c3c5f9776d763d
bigsparsh <sparshsingh7586@gmail.com>
2023-06-18 19:21:49 +0530

    Fixed reading csv again

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
+df = pd.read_csv('dictionary.csv')
 
 
 @app.route("/")
@@ -11,7 +12,6 @@ def home():
 
 @app.route("/api/v1/<word>")
 def dictionary(word):
-    df = pd.read_csv('dictionary.csv')
     definition = df['definition'].loc[df['word'] == word].squeeze()
     return {"word": str(word),
             "definition": definition}
